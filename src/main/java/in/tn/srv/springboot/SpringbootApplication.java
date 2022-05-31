72ee9de0b0882fd45047f974eb6bf16cf1be1f2c
vinodrajalingam <vinodrajalingam@gmail.com>
2022-05-31 17:30:27 +0530

    Spring Boot with DataJPA

--- src/main/java/in/tn/srv/springboot/SpringbootApplication.java
+++ src/main/java/in/tn/srv/springboot/SpringbootApplication.java
@@ -1,7 +1,11 @@
 package in.tn.srv.springboot;
 
+import in.tn.srv.springboot.entitycontainer.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @SpringBootApplication
 public class SpringbootApplication {
--- src/main/java/in/tn/srv/springboot/requesthandlerscontainer/RequestsHandler.java
+++ src/main/java/in/tn/srv/springboot/requesthandlerscontainer/RequestsHandler.java
@@ -1,13 +1,50 @@
 package in.tn.srv.springboot.requesthandlerscontainer;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import in.tn.srv.springboot.entitycontainer.UserEntity;
+import in.tn.srv.springboot.repocontainer.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class RequestsHandler {
+    @Autowired
+    UserRepo userRepo;
 
+    //@Autowired
+    //UserEntity userEntity;
     @RequestMapping("/")
     public String welcomeUser() {
         return "Welcome User";
+    }
+
+    @PostMapping("/save")
+    public UserEntity saveUser(@RequestBody UserEntity userEntity) {
+
+        return userRepo.save(userEntity);
+    }
+
+    @PostMapping("/saveAll")
+    public List<UserEntity> saveAll(@RequestBody List<UserEntity> userEntities) {
+
+        //UserEntity userEntity = userEntities.get(0);
+        //UserEntity userEntity2 = userEntities.get(1);
+        //return userRepo.saveAll(List.of(userEntity,userEntity2));
+        return (List<UserEntity>) userRepo.saveAll(userEntities);
+    }
+    @RequestMapping("/users")
+    public List<UserEntity> getUsers() {
+        return userRepo.findAll();
+    }
+    @RequestMapping("/user/{id}")
+    public Optional<UserEntity> getUser(@PathVariable("id") int ID) {
+        return userRepo.findById(ID);
+    }
+    @RequestMapping("/users/{uName}")
+    public Optional<UserEntity> getUser(@PathVariable("uName") String userName) {
+        return userRepo.findByfirstName(userName);
     }
 }
